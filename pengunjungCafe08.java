877211a2779f87749e0c5b80f970c209d2a150e5
Your Name <you@example.com>
2024-11-21 16:03:34 +0700

    percobaan 4

---
+++ pengunjungCafe08.java
@@ -0,0 +1,12 @@
+public class pengunjungCafe08 {
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar nama pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        daftarPengunjung("Andi", "Budi", "Citra");
+    }
+}
