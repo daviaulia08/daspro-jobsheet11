928729dcde90662abbba5afa02f309235d5ccc22
Your Name <you@example.com>
2024-11-21 14:37:55 +0700

    modifikasi pertanyaan 2

--- hitungTotalHarga08.java
+++ hitungTotalHarga08.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class hitungTotalHarga08 {
-    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+    public static void hitungTotalHarga(int pilihanMenu, int banyakItem, String kodePromo) {
         int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
 
-        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;
+        double hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        double diskon, hargaDiskon;
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Selamat anda mendapat diskon sebesar 50% !");
+            diskon = 0.50;
+            hargaDiskon = hargaTotal - (hargaTotal*diskon);
+            System.out.println("Total harga yang harus dibayar adalah : Rp " + hargaDiskon);
+        }
+        else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Selamat anda mendapat diskon sebesar 30% !");
+            diskon = 0.30;
+            hargaDiskon = hargaTotal - (hargaTotal*diskon);
+            System.out.println("Total harga yang harus dibayar adalah : Rp " + hargaDiskon);
+        }
+        else {
+            System.out.println("Maaf, kode promo anda tidak valid");
+            diskon = 0;
+            hargaDiskon = hargaTotal - (hargaTotal*diskon);
+            System.out.println("Total harga yang harus dibayar adalah : Rp " + hargaDiskon);
+        }
     }
 
     public static void main(String[] args) {
@@ -14,8 +33,10 @@ public class hitungTotalHarga08 {
         int pilihanMenu = sc.nextInt();
         System.out.print("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
-        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+        sc.nextLine();
+        System.out.print("Masukkan kode promo: ");
+        String kodePromo = sc.nextLine();
 
-        System.out.println("Total harga untuk pesanan Anda : Rp" + totalHarga);
+        hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
     }
 }
