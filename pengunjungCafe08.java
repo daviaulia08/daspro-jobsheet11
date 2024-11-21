6e14ce471efe664fc3db2d3edff08e60883a5098
Your Name <you@example.com>
2024-11-21 16:05:14 +0700

    modifikasi pertanyaan 2

--- pengunjungCafe08.java
+++ pengunjungCafe08.java
@@ -1,8 +1,8 @@
 public class pengunjungCafe08 {
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String x : namaPengunjung) {
+            System.out.println("- " + x);
         }
     }
 
